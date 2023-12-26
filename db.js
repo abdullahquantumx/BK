85d97b5b732c6a81e8b3f0399d5331e47ecea9fe
abdullahquantumx <137316478+abdullahquantumx@users.noreply.github.com>
2023-12-26 18:48:32 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://127.0.0.1:27017/notebook';
+const mongoURI = 'mongodb+srv://new_user21:mongodb@mongocluster.yjukacp.mongodb.net/';
 
 const connectToMongo = async () => {
   try {
